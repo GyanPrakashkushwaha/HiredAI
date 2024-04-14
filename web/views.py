286fc0559b1270f327ca78e3c0dca761c18b90e3
GyanPrakashkushwaha <gyanp7880@gmail.com>
2024-04-14 17:15:21 +0530

    add templates and static files

--- web/views.py
+++ web/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Create your views here.
+def home(request):
+    return render(request=request,template_name='index.html')
+
+def forYou(request):
+    return render(request=request,template_name='foryou.html')
