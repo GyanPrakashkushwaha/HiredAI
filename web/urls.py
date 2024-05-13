9ba157d86d488ceab43aa49384635896c5e4060e
GyanPrakashkushwaha <gyanp7880@gmail.com>
2024-05-13 20:31:23 +0530

    add all fiels

--- web/urls.py
+++ web/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path(route='recommendation/',view=views.recommendation,name='recommendation'),
     path(route='profile/',view=views.profile,name='profile'),
     path(route='about/',view=views.about,name='scrap'),
-    path('home/', views.home_view )
 ]
--- web/views.py
+++ web/views.py
@@ -18,12 +18,18 @@ def profile(request):
 def about(request):
     return render(request=request,template_name='scrap.html')
 
+def handle_uploaded_file(f):
+    with open('assets/resumes/'+f.name,'wb+') as file_:
+        for chunk in f.chunks():
+            file_.write(chunk)      
+
 def upload(request):
     context = {}
-    context['form'] = ResumeForm()
+    if request.POST:
+        form = ResumeForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES["resume_field"])
+    else:
+        form = ResumeForm()
+    context['form'] = form
     return render( request , "upload.html",context)
-
-def home_view(request): 
-    context = {} 
-    context['form'] = GeeksForm() 
-    return render( request, "home.html", context)
